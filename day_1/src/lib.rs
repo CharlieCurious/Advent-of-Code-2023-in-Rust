5d9fcc9434d421f152a39557b54e99298bdf08de
CharlieCurious <dfrco2@outlook.com>
2024-06-24 22:46:55 +0300

    added input.txt

---
+++ day_1/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn get_calibrations_sum(file_name: &str) -> i32 {
+    panic!("Not implemented")
+}
+
+fn get_calibration_from_line(line: &str) -> i32 {
+    panic!("Not implemented")
+}
+
+#[cfg(test)]
+mod tests {
+
+}
