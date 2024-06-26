2d38b4c4c1efaa2818d91361bb6dad9ceb3d646f
CharlieCurious <dfrco2@outlook.com>
2024-06-26 23:06:13 +0300

    style change + gitignore

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,7 +4,7 @@ use day_1::get_calibrations_sum;
 
 fn main() {
     let file_path = env::args().nth(1)
-    .expect("Did no provide file path argument");
+        .expect("Did no provide file path argument");
     let file_string = fs::read_to_string(file_path)
         .expect("Should have been able to read file");
     let sum = get_calibrations_sum(file_string);
