802017f2ce143ba252e3af5873ae316414f195d8
CharlieCurious <dfrco2@outlook.com>
2024-06-26 22:46:13 +0300

    day 1, part 1, bug fix

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -5,8 +5,7 @@ pub fn get_calibrations_sum(file_string: String) -> u32 {
 }
 
 fn get_calibration_from_line(line: &str) -> u32 {
-    let line_chars: Vec<char> = line.chars()
-        .collect();
+    let line_chars: Vec<char> = line.chars().collect();
 
     let mut start = 0;
     let mut end = line.len() - 1;
@@ -14,10 +13,9 @@ fn get_calibration_from_line(line: &str) -> u32 {
     let mut first_number: Option<char> = None;
     let mut last_number: Option<char> = None;
 
-    while start <= end {
+    while start <= line.len()-1 {
         if first_number.is_none() {
-            let start_ch = *line_chars.get(start)
-                .unwrap();
+            let start_ch = line_chars[start];
             if start_ch.is_digit(10) {
                 first_number = Some(start_ch)
             }
@@ -25,8 +23,7 @@ fn get_calibration_from_line(line: &str) -> u32 {
         }
 
         if last_number.is_none() {
-            let end_ch: char = *line_chars.get(end)
-                .unwrap();
+            let end_ch: char = line_chars[end];
             if end_ch.is_digit(10) {
                 last_number = Some(end_ch)
             }
@@ -40,7 +37,6 @@ fn get_calibration_from_line(line: &str) -> u32 {
         }
 
     }
-
     let first_number = first_number
         .expect("No numbers found on the string. All strings must have at least one number.");
     let last_number = last_number.unwrap_or(first_number);
@@ -67,13 +63,17 @@ mod tests {
         let case0 = get_calibration_from_line("1abc2");
         let case1 = get_calibration_from_line("pqr3stu8vwx");
         let case2 = get_calibration_from_line("a1b2c3d4e5f");
-        let case3 = get_calibration_from_line("treb7uchet");
+        let case3 = get_calibration_from_line("t7rebuchet");
+        let case4 = get_calibration_from_line("5charlie");
+        let case5 = get_calibration_from_line("eightg1");
 
         // assert
         assert_eq!(12, case0);
         assert_eq!(38, case1);
         assert_eq!(15, case2);
         assert_eq!(77, case3);
+        assert_eq!(55, case4);
+        assert_eq!(11, case5);
     }
 
     #[test]
